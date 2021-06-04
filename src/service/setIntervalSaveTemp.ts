f579fdcf0a2d4c8bc10226bf27caf88862dd0c79
Wilk Franca <wilk.caetano@gmail.com>
2021-06-04 00:05:42 -0300

    add 3,54e+6 in setTimeout

--- src/service/setIntervalSaveTemp.ts
+++ src/service/setIntervalSaveTemp.ts
@@ -39,7 +39,7 @@ async function setTimeSaveTemp(cities: string, dates: Date) {
 }
 
 const interval = async (verify: number) => {
-    const interval = setTimeout(setTimeSaveTemp, 3600000)
+    const interval = setTimeout(setTimeSaveTemp, 3,54e+6)
     if (verify === 0) clearTimeout(interval)
 
 }
